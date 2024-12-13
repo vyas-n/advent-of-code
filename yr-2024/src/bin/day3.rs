718628147f148168e0ecde5546481270e8b937b4
Vyas Nellutla <me@vyas-n.com>
2024-12-13 11:27:31 -0600

    feat: part2

--- yr-2024/src/bin/day3.rs
+++ yr-2024/src/bin/day3.rs
@@ -3,12 +3,6 @@ use std::path::Path;
 
 use regex::Regex;
 
-fn const_contents() -> String {
-    let contents: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-
-    contents.to_string()
-}
-
 fn file_contents() -> String {
     let file_path: &Path = Path::new("inputs/day3.txt");
 
@@ -26,27 +20,27 @@ fn file_contents() -> String {
 fn main() {
     let contents: String = file_contents();
 
-    println!("Answer: {}", part1(&contents))
-}
-
-struct Mul {
-    num1: i32,
-    num2: i32,
-}
-
-impl From<&str> for Mul {
-    fn from(value: &str) -> Self {
-        let re = Regex::new(r"[0-9]+").unwrap();
-        let mut iter = re.find_iter(value);
-
-        Mul {
-            num1: iter.next().unwrap().as_str().parse().unwrap(),
-            num2: iter.next().unwrap().as_str().parse().unwrap(),
-        }
-    }
+    println!("Answer: {}", part2(&contents))
 }
 
 fn part1(input: &str) -> i32 {
+    struct Mul {
+        num1: i32,
+        num2: i32,
+    }
+
+    impl From<&str> for Mul {
+        fn from(value: &str) -> Self {
+            let re = Regex::new(r"[0-9]+").unwrap();
+            let mut iter = re.find_iter(value);
+
+            Mul {
+                num1: iter.next().unwrap().as_str().parse().unwrap(),
+                num2: iter.next().unwrap().as_str().parse().unwrap(),
+            }
+        }
+    }
+
     let re: Regex = Regex::new(r"mul\([0-9]+\,[0-9]+\)").unwrap();
 
     re.find_iter(input)
@@ -56,7 +50,68 @@ fn part1(input: &str) -> i32 {
 }
 
 fn part2(input: &str) -> i32 {
-    todo!("Setup fn part2")
+    enum Instruction {
+        Mul { num1: i32, num2: i32 },
+        Do { enabled: bool },
+    }
+
+    impl From<&str> for Instruction {
+        fn from(value: &str) -> Self {
+            if value.starts_with("mul") {
+                let re = Regex::new(r"[0-9]+").unwrap();
+                let mut iter = re.find_iter(value);
+
+                Instruction::Mul {
+                    num1: iter.next().unwrap().as_str().parse().unwrap(),
+                    num2: iter.next().unwrap().as_str().parse().unwrap(),
+                }
+            } else if value.starts_with("don't") {
+                Instruction::Do { enabled: false }
+            } else if value.starts_with("do") {
+                Instruction::Do { enabled: true }
+            } else {
+                panic!("This string shouldn't have been parsed.")
+            }
+        }
+    }
+
+    struct Answer {
+        enabled: bool,
+        sum: i32,
+    }
+
+    let re = Regex::new(r"mul\([0-9]+\,[0-9]+\)|don\'t\(\)|do\(\)").unwrap();
+
+    re.find_iter(input)
+        .map(|substring| {
+            println!("{}", substring.as_str());
+            let inst = Instruction::from(substring.as_str());
+
+            inst
+        })
+        .fold(
+            Answer {
+                enabled: true,
+                sum: 0,
+            },
+            |answer, instruction| match instruction {
+                Instruction::Mul { num1, num2 } => {
+                    if answer.enabled {
+                        Answer {
+                            enabled: answer.enabled,
+                            sum: answer.sum + (num1 * num2),
+                        }
+                    } else {
+                        answer
+                    }
+                }
+                Instruction::Do { enabled } => Answer {
+                    enabled,
+                    sum: answer.sum,
+                },
+            },
+        )
+        .sum
 }
 
 #[cfg(test)]
@@ -65,7 +120,8 @@ mod tests {
 
     #[test]
     fn part1_input1() {
-        let contents: String = const_contents();
+        let contents: String =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))".to_string();
 
         assert_eq!(part1(&contents), 161)
     }
@@ -77,17 +133,18 @@ mod tests {
         assert_eq!(part1(&contents), 184122457)
     }
 
-    // #[test]
-    // fn part2_input1() {
-    //     let contents: String = const_contents();
+    #[test]
+    fn part2_input1() {
+        let contents: String =
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".to_string();
 
-    //     assert_eq!(part2(&contents), todo!("Setup test output 1 for part2"))
-    // }
+        assert_eq!(part2(&contents), 48)
+    }
 
-    // #[test]
-    // fn part2_input2() {
-    //     let contents = file_contents();
+    #[test]
+    fn part2_input2() {
+        let contents = file_contents();
 
-    //     assert_eq!(part2(&contents), todo!("Setup test output 2 for part2"))
-    // }
+        assert_eq!(part2(&contents), 107862689)
+    }
 }
